d2e528ecde9b22cbde5d2e7444a8bf10bb339e44
Brian Rosner <brosner@gmail.com>
2010-01-23 16:51:44 -0700

    pulled over old quotes model

--- pinaxsite_project/apps/quotes/models.py
+++ pinaxsite_project/apps/quotes/models.py
@@ -1,1 +1,11 @@
+from datetime import datetime
+
 from django.db import models
+
+
+
+class Quote(models.Model):
+    
+    text = models.TextField()
+    author = models.CharField(max_length=100)
+    added = models.DateTimeField(default=datetime.now)
