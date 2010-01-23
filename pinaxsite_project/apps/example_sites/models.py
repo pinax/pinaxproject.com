b83f39b057992c2d20d45e81b5d995885f37b486
Brian Rosner <brosner@gmail.com>
2010-01-23 16:30:11 -0700

    Site.title -> Site.name

--- pinaxsite_project/apps/example_sites/models.py
+++ pinaxsite_project/apps/example_sites/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Site(models.Model):
     
-    title = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to="sites")
     url = models.URLField(verify_exists=False)
