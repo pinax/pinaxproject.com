02c2d8aae8abbce62228f920a153aa2dc78b5b40
Brian Rosner <brosner@gmail.com>
2010-01-23 16:12:19 -0700

    added some admin.py files

---
+++ pinaxsite_project/apps/downloads/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from downloads.models import Release, ReleaseFile
+
+
+
+class ReleaseFileInline(admin.StackedInline):
+    model = ReleaseFile
+
+
+class ReleaseAdmin(admin.ModelAdmin):
+    inlines = [
+        ReleaseFileInline,
+    ]
+
+
+
+admin.site.register(ReleaseAdmin)
---
+++ pinaxsite_project/apps/example_sites/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from example_sites.models import Site
+
+
+
+admin.site.register(Site)
