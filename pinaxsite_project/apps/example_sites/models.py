68652edc7738fd7887cb3c38788e4f81d6a8fbc6
Brian Rosner <brosner@gmail.com>
2010-01-23 16:07:50 -0700

    Site model

--- pinaxsite_project/apps/example_sites/models.py
+++ pinaxsite_project/apps/example_sites/models.py
@@ -1,1 +1,17 @@
+from datetime import datetime
+
 from django.db import models
+
+
+
+class Site(models.Model):
+    
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="sites")
+    url = models.URLField(verify_exists=False)
+    approved = models.BooleanField()
+    created = models.DateTimeField(default=datetime.now)
+    when_approved = models.DateTimeField()
+    contact_name = models.CharField(max_length=100, blank=True)
+    contact_email = models.EmailField(blank=True)
