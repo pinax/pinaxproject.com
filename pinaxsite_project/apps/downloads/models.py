be9dc832bfe1e8dc27ce439bc61db9c127397fe5
Brian Rosner <brosner@gmail.com>
2010-01-23 15:59:55 -0700

    added downloads models

--- pinaxsite_project/apps/downloads/models.py
+++ pinaxsite_project/apps/downloads/models.py
@@ -1,1 +1,18 @@
+from datetime import datetime
+
 from django.db import models
+
+
+
+class Release(models.Model):
+    
+    version = models.CharField(max_length=10)
+    stable = models.BooleanField()
+    timestamp = models.DateTimeField(default=datetime.now)
+
+
+class ReleaseFile(models.Model):
+    
+    file = models.FileField()
+    md5_hash = models.CharField(max_length=32)
+    sha1_hash = models.CharField(max_length=40)
