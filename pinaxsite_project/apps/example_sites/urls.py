35b03874288518a154906e0f9f50e0e8aea626d8
Brian Rosner <brosner@gmail.com>
2010-01-23 16:33:05 -0700

    updated sites_home template

--- pinaxsite_project/apps/example_sites/urls.py
+++ pinaxsite_project/apps/example_sites/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns("",
     url(r"^$", direct_to_template, {
-        "template": "sites.html",
+        "template": "example_sites/home.html",
     }, name="sites_home"),
     url(r"^submit/$", "example_sites.views.submit_site", name="sites_submit_site"),
 )
