8d44f766348eda91caa0c7af29dbb4e4b11c8b12
Brian Rosner <brosner@gmail.com>
2010-01-23 11:35:41 -0700

    moved to using views provided by Pinax

--- pinaxsite_project/urls.py
+++ pinaxsite_project/urls.py
@@ -9,23 +9,7 @@ admin.autodiscover()
 
 
 
-# override the default handler500 so i can pass MEDIA_URL
-handler500 = "company_project.views.server_error"
-
-
-def static_view(request, path):
-    """
-    serve pages directly from the templates directories.
-    """
-    if not path or path.endswith("/"):
-        template_name = path + "index.html"
-    else:
-        template_name = path
-    return render_to_response(template_name, RequestContext(request))
-
-
-def noop(request):
-    pass
+handler500 = "pinax.views.server_error"
 
 
 urlpatterns = patterns("",
@@ -36,7 +20,7 @@ urlpatterns = patterns("",
     url(r"^feed/(?P<section>[-\w]+)/$", "biblion.views.blog_feed", name="blog_feed"),
     
     # stubbed out for reverse (webserver maps this to static file serving)
-    url(r"^docs/$", noop, name="documentation"),
+    url(r"^docs/$", "pinax.views.noop", name="documentation"),
     
     url(r"^sites/", include("example_sites.urls")),
     url(r"^quotes/", include("quotes.urls")),
@@ -53,5 +37,5 @@ if settings.SERVE_MEDIA:
 
 
 urlpatterns += patterns("",
-    (r"^(.*)$", static_view),
+    (r"^(.*)$", "pinax.views.static_view"),
 )
