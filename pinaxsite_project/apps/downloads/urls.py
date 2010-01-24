491f2fecfa104cd126583631c3248987f8f275fa
Brian Rosner <brosner@gmail.com>
2010-01-23 17:35:45 -0700

    updated downloads template path

--- pinaxsite_project/apps/downloads/urls.py
+++ pinaxsite_project/apps/downloads/urls.py
@@ -5,6 +5,6 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns("",
     url(r"^$", direct_to_template, {
-        "template": "downloads.html",
+        "template": "downloads/home.html",
     }, name="downloads_home"),
 )
