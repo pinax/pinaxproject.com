6528cd2be8e5e061b24b59d8fc9dafaa8b06f98b
Brian Rosner <brosner@gmail.com>
2010-01-24 09:42:08 -0700

    fixed RandomSitesNode.handle_token to work properly

--- pinaxsite_project/apps/example_sites/templatetags/example_sites_tags.py
+++ pinaxsite_project/apps/example_sites/templatetags/example_sites_tags.py
@@ -32,14 +32,14 @@ class RandomSitesNode(template.Node):
     def handle_token(cls, parser, token, **kwargs):
         bits = token.split_contents()
         
-        if len(bits) != 3:
-            raise template.TemplateSyntaxError("%r takes exactly two arguments "
-                "(first argument must be 'as')" % bits[0])
-        if bits[1] != "as":
+        if len(bits) != 4:
+            raise template.TemplateSyntaxError("%r takes exactly three "
+                "arguments (first argument must be 'as')" % bits[0])
+        if bits[2] != "as":
             raise template.TemplateSyntaxError("Second argument to %r must be "
                 "'as'" % bits[0])
         
-        return cls(bits[2], **kwargs)
+        return cls(bits[1], bits[3] **kwargs)
     
     def __init__(self, limit, context_var):
         self.limit = template.Variable(limit)
