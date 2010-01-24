60e4ffcf3c29dbfee7fea55a53f64c151f6cb4ee
Brian Rosner <brosner@gmail.com>
2010-01-24 09:50:26 -0700

    added singleton feature instance in Quote

--- pinaxsite_project/apps/quotes/models.py
+++ pinaxsite_project/apps/quotes/models.py
@@ -10,3 +10,8 @@ class Quote(models.Model):
     author = models.CharField(max_length=100)
     added = models.DateTimeField(default=datetime.now)
     featured = models.BooleanField(default=False)
+    
+    def save(self, **kwargs):
+        if self.featured:
+            Quote.objects.filter(featured=True).update(featured=False)
+        super(Site, self).save(**kwargs)
