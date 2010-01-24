974f7e897049bec0e7b6952dd968c40c949af057
Brian Rosner <brosner@gmail.com>
2010-01-23 17:33:25 -0700

    added downloads template tags

---
+++ pinaxsite_project/apps/downloads/templatetags/downloads_tags.py
@@ -0,0 +1,71 @@
+from django import template
+
+
+
+register = template.Library()
+
+
+
+class BaseReleaseNode(template.Node):
+    
+    @classmethod
+    def handle_token(cls, parser, token, **kwargs):
+        bits = token.split_contents()
+        
+        if len(bits) != 3:
+            raise template.TemplateSyntaxError("%r takes exactly two arguments "
+                "(first argument must be 'as')" % bits[0])
+        if bits[1] != "as":
+            raise template.TemplateSyntaxError("Second argument to %r must be "
+                "'as'" % bits[0])
+        
+        return cls(bits[2], **kwargs)
+
+
+class LatestReleaseNode(BaseReleaseNode):
+    
+    def __init__(self, context_var, kind):
+        self.context_var = context_var
+        self.kind = kind
+    
+    def render(self, context):
+        
+        if self.kind == "stable":
+            queryset = Release.objects.filter(stable=True)
+        elif self.kind == "development":
+            queryset = Release.objects.filter(stable=False)
+        else:
+            raise ValueError("Unknown kind in LatestReleaseNode.render")
+        
+        queryset = queryset.order_by("-timestamp")
+        context[self.context_var] = queryset[0]
+        
+        return u""
+
+
+class OlderReleasesNode(BaseReleaseNode):
+    
+    def __init__(self, context_var):
+        self.context_var = context_var
+    
+    def render(self, context):
+        
+        queryset = Release.objects.order_by("-timestamp")[2:]
+        context[self.context_var] = queryset
+        
+        return u""
+
+
+@register.tag
+def latest_stable_release(parser, token):
+    return LatestReleaseNode.handle_token(parser, token, kind="stable")
+
+
+@register.tag
+def latest_development_release(parser, token):
+    return LatestReleaseNode.handle_token(parser, token, kind="development")
+
+
+@register.tag
+def older_releases(parser, token):
+    return OlderReleasesNode.handle_token(parser, token)
