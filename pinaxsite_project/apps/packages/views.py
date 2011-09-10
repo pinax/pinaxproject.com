f28c8d3f49d00658ec16fd31e9bc415c5d5d53e4
Patrick Altman <paltman@gmail.com>
2011-09-09 17:46:46 -0700

    Bring back the static image punchcard for now

--- pinaxsite_project/apps/packages/views.py
+++ pinaxsite_project/apps/packages/views.py
@@ -1,7 +1,5 @@
 import datetime
 
-from math import log
-
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
@@ -64,15 +62,39 @@ class PullRequestList(ListView):
     context_object_name = "pull_requests"
 
 
-def normalize(value, max_value):
-    if value == 0 or max_value == 0:
-        return 0
-    return int(round(25 * log(value) / log(max_value)))
-
-
 class DashboardView(TemplateView):
     
     template_name = "packages/dashboard.html"
+    
+    def punchcard_url(self, commits, months):
+        url = "https://chart.googleapis.com/chart?chs=550x%s&cht=s&" % (30 * len(commits))
+        m = "|".join([x.strftime("%b") for x in months])
+        obj = "|".join([
+            x.name for x,y in commits.iteritems()
+        ])
+        chxl = "chxl=0:||%s||1:||%s|&" % (m, obj)
+        
+        url += chxl
+        
+        first = ["0"]
+        second = ["0"]
+        third = ["0"]
+        for i, key in enumerate(commits):
+            for j, commit in enumerate(commits[key]["commits"]):
+                first.append(str(j))
+                second.append(str(i))
+                third.append(str(commit["count"]))
+        chd = "t:%s|%s|%s" % (",".join(first), ",".join(second), ",".join(third))
+        
+        url += "chd=%s&" % chd
+        
+        chds = "-1,%s,-1,%s,0,17" % (len(months), len(commits))
+        chm = "o,333333,1,-1,22"
+        chxt = "x,y"
+        
+        url += "chds=%s&chm=%s&chxt=%s&" % (chds, chm, chxt)
+        url += "chf=bg,s,efefef"
+        return url
     
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
@@ -82,13 +104,46 @@ class DashboardView(TemplateView):
             for x in final_months(6)
         ]
         
+        six_months.reverse()
+        
         context["months"] = six_months
         
-        context["author_commits"] = CommitsByAuthorByMonth.objects.filter(
+        author_commits_qs = CommitsByAuthorByMonth.objects.filter(
             month__gte=six_months[0]
-        ).order_by("author__name", "-month")
-        context["package_commits"] = CommitsByPackageByMonth.objects.filter(
+        ).order_by("author__pk", "month").select_related()
+
+        author_commits = {}
+        for commit in author_commits_qs.all():
+            if author_commits.get(commit.author) is None:
+                author_commits[commit.author] = {"commits": []}
+            author_commits[commit.author]["commits"].append({
+                "month": commit.month,
+                "count": commit.commit_count
+            })
+        for author in author_commits.keys():
+            if sum([x["count"] for x in author_commits[author]["commits"]]) == 0:
+                author_commits.pop(author) # don't show inactives
+        
+        package_commits_qs = CommitsByPackageByMonth.objects.filter(
             month__gte=six_months[0]
-        ).order_by("package__name", "-month")
+        ).order_by("package__name", "-month").select_related()
+        
+        package_commits = {}
+        for commit in package_commits_qs.all():
+            if package_commits.get(commit.package) is None:
+                package_commits[commit.package] = {"commits": []}
+            package_commits[commit.package]["commits"].append({
+                "month": commit.month,
+                "count": commit.commit_count
+            })
+        for package in package_commits.keys():
+            if sum([x["count"] for x in package_commits[package]["commits"]]) == 0:
+                package_commits.pop(package) # don't show inactives
+        
+        context["author_commits"] = author_commits
+        context["package_commits"] = package_commits
+        
+        context["author_punchcard_url"] = self.punchcard_url(author_commits, six_months)
+        context["package_punchcard_url"] = self.punchcard_url(package_commits, six_months)
         
         return context
