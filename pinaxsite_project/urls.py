7cb8181d8a4f291c5588c801aa99431a7e32f223
Brian Rosner <brosner@gmail.com>
2011-12-05 11:27:43 -0700

    redirect to readthedocs

--- pinaxsite_project/urls.py
+++ pinaxsite_project/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import direct_to_template, redirect_to
 
 from django.contrib import admin
 admin.autodiscover()
@@ -14,28 +14,16 @@ handler500 = "pinax.views.server_error"
 
 urlpatterns = patterns("",
     url(r"^$", direct_to_template, {"template": "homepage.html"}, name="home"),
-    
     url(r"^blog/", include("biblion.urls")),
     url(r"^feed/$", "biblion.views.blog_feed", name="blog_feed_combined"),
     url(r"^feed/(?P<section>[-\w]+)/$", "biblion.views.blog_feed", name="blog_feed"),
-    url(r"^docs/", include("docs.urls")),
-    
+    url(r"^docs/.*", redirect_to, {"url": "http://pinax.readthedocs.org/"}),
     url(r"^downloads/", include("downloads.urls")),
     url(r"^sites/", include("example_sites.urls")),
     url(r"^quotes/", include("quotes.urls")),
-    url(r"^events/$", direct_to_template, {
-        "template": "events/home.html",
-    }, name="events_home"),
-    
+    url(r"^events/$", direct_to_template, {"template": "events/home.html"}, name="events_home"),
     url(r"^admin/", include(admin.site.urls)),
-    
     url(r"^ecosystem/", include("packages.urls")),
-    
-    # temporary urls
-    url(r"^docs/$", direct_to_template, {"template": "docs/index.html"}, name="index"),
-    url(r"^docs/dev/$", direct_to_template, {"template": "docs/dev_index.html"}, name="dev_index"),
-    url(r"^docs/dev/gettingstarted/$", direct_to_template, {"template": "docs/detail.html"}, name="gettingstarted"),
-    
 )
 
 
